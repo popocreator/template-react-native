4c4c4fc3c804a741f2bf0a85bb1a24e733eaa5e2
Jiseong Hong <jisunghong@Jiseongs-MacBook-Pro.local>
2022-05-09 02:19:25 +0900

    Added sample component files

---
+++ src/components/Dialog/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+type Props = {};
+
+export default function Dialog({}: Props) {
+  return (
+    <View>
+      <Text>Dialog</Text>
+    </View>
+  );
+}
---
+++ src/components/DraggableList/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+type Props = {};
+
+export default function DraggableList({}: Props) {
+  return (
+    <View>
+      <Text>DraggableList</Text>
+    </View>
+  );
+}
